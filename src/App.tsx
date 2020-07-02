4d2440b4c3c3351025d17eab4890298b6ec341c4
cvrajeesh <cvrajeesh@gmail.com>
2020-07-02 15:46:54 +0530

    feat: compass component using deviceorientation event

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Compass from "./Compass";
+import { GitHubIcon, InfoIcon, CloseIcon } from "./icons";
 
-function App() {
+const InfoPanel: React.FC = (props) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  return (
+    <div className="info">
+      {isDialogOpen && (
+        <div className="dialog-container">
+          <div className="dialog">
+            <div
+              className="close-dialog"
+              onClick={() => setIsDialogOpen(false)}
+            >
+              <CloseIcon />
+            </div>
+            <div className="content">
+              <p>
+                This demo uses <strong>deviceorientation</strong> event to get
+                data from device sensor. If device sensor is not calibrated
+                correctly, you will see incorrect reading. Please Google to find
+                out calibrating your device sensor
+              </p>
+
+              <p>
+                Lean more about this event from{" "}
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/API/Window/deviceorientation_event"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  here
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="panel">
+        <div className="item">
+          Hosted by{" "}
+          <a
+            href="https://www.clodui.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            clodui.com
+          </a>
+        </div>
+        <div className="item">
+          <a
+            href="https://github.com/cvrajeesh"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon />
+          </a>
+        </div>
+        <div
+          className="item action"
+          onClick={() => setIsDialogOpen((prev) => !prev)}
+        >
+          <InfoIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Compass />
+      </div>
+      <InfoPanel />
     </div>
   );
 }
-
-export default App;
